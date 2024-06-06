8d4cb5bca36c10938da803e10ad4e82fb89e35c2
daymade <daymadev89@gmail.com>
2024-06-06 19:46:30 +0800

    feat: add semantic error code to channel gateway exception
    Signed-off-by: daymade <daymadev89@gmail.com>

--- src/main/java/tech/typeof/backend/gateway/channel/adapter/api/exception/ChannelGatewayException.java
+++ src/main/java/tech/typeof/backend/gateway/channel/adapter/api/exception/ChannelGatewayException.java
@@ -1,11 +1,24 @@
 package tech.typeof.backend.gateway.channel.adapter.api.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class ChannelGatewayException extends RuntimeException {
+    private final HttpStatus httpStatus;
+
     public ChannelGatewayException(String message) {
         super(message);
+        this.httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
     }
 
-    public ChannelGatewayException(String message, RuntimeException cause) {
+    public ChannelGatewayException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public ChannelGatewayException(HttpStatus httpStatus, String message, Exception cause) {
         super(message, cause);
+        this.httpStatus = httpStatus;
     }
 }
